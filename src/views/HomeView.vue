29a74370ff84cfed9235a3e4af70639bbc2f85bb
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2024-04-24 14:24:54 +0800

    added summary card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,37 +1,58 @@
 <script setup lang="ts">
 import SummaryCardView from '../components/SummaryCardView.vue'
+import LoadingView from '../components/reusable/LoadingView.vue'
 
 import type { VueCookies } from 'vue-cookies'
 
 import { RouterLink } from 'vue-router'
-import { inject, onMounted, ref } from 'vue'
+import { computed, inject, onMounted, ref } from 'vue'
 import { jwtDecode } from 'jwt-decode'
 
 const $cookies = inject<VueCookies>('$cookies') as VueCookies
 const claims = jwtDecode($cookies.get('jwt')) as any
 
-const loaded = ref(false)
+const loadedStatus = ref([] as boolean[])
+const fetching = ref(true)
+const loaded = computed(() => {
+  if (loadedStatus.value.length === 0) {
+    return true
+  }
 
-const patients = ref<Patient[]>()
+  return loadedStatus.value.reduce((a, b) => a && b)
+})
+
+const patients = ref<Patient[]>([])
 onMounted(async () => {
+  fetching.value = true
   const response = await fetch(`/dialife-api/doctor/of/${claims['id']}`)
   const data = await response.json()
 
   patients.value = data
-  loaded.value = true
+  loadedStatus.value = Array(patients.value.length).fill(false)
+  fetching.value = false
 })
 </script>
 
 <template>
+  <LoadingView :loading="!loaded || fetching" />
   <div class="container">
     <div class="home">
       <h3>Dashboard</h3>
       <div class="summary-container">
-        <template v-if="!patients?.length">
+        <template v-if="!patients?.length && !fetching">
           <h5>(No Patients)</h5>
           <RouterLink class="no-data" to="/records">Add Patient</RouterLink>
         </template>
-        <SummaryCardView v-for="patient in patients" :patient :key="patient.patient_id" />
+        <SummaryCardView
+          v-for="(patient, index) in patients"
+          :on-load="
+            () => {
+              loadedStatus[index] = true
+            }
+          "
+          :patient
+          :key="patient.patient_id"
+        />
       </div>
     </div>
   </div>
